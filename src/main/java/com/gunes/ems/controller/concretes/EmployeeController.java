58b5e94a5c72d07f008095d520c60ca8467e8b73
gunesserkan <gunesserkan883@gmail.com>
2024-05-24 23:53:26 +0300

    Added getEmployeeById

--- src/main/java/com/gunes/ems/controller/concretes/EmployeeController.java
+++ src/main/java/com/gunes/ems/controller/concretes/EmployeeController.java
@@ -1,14 +1,12 @@
 package com.gunes.ems.controller.concretes;
 
 import com.gunes.ems.dto.concretes.EmployeeDto;
+import com.gunes.ems.mapper.concretes.EmployeeMapper;
 import com.gunes.ems.service.abstracts.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -23,7 +21,10 @@ public class EmployeeController {
        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
-
-
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
+       EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+       return ResponseEntity.ok(employeeDto);
+    }
 
 }
